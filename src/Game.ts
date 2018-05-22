fbd03824107e7d009dcbf93d56b0493dd911da2b
Nokitoo <guillaume.labey@epitech.eu>
2018-05-22 23:00:23 +0900

    Remove commented code

--- src/Game.ts
+++ src/Game.ts
@@ -11,14 +11,5 @@ export default class Game extends Phaser.Game {
             scene: Level1
         };
         super(config)
-
-        //this.state.add('Level1', Level1);
-
-       // super(800, 600, Phaser.AUTO, 'content', null);
-
-/*         this.state.add('Level1', Level1, false);
-
-        this.state.start('Level1'); */
     }
-
 }
